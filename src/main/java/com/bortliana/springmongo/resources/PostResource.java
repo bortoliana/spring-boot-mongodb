6031749e93397dfc78e570ba9f565bca6a7de4c4
ana <analeticiadebortoli@gmail.com>
2023-01-21 17:38:20 -0300

    Obtendo um post por id

---
+++ src/main/java/com/bortliana/springmongo/resources/PostResource.java
@@ -0,0 +1,21 @@
+package com.bortliana.springmongo.resources;
+
+import com.bortliana.springmongo.domain.Post;
+import com.bortliana.springmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/v1/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
