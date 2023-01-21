bea440967b590e395ca981dae8fc654fe8325d92
ana <analeticiadebortoli@gmail.com>
2023-01-21 16:09:03 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/bortliana/springmongo/resources/UserResource.java
+++ src/main/java/com/bortliana/springmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.bortliana.springmongo.resources;
 
+import com.bortliana.springmongo.domain.Post;
 import com.bortliana.springmongo.domain.User;
 import com.bortliana.springmongo.dto.UserDTO;
 import com.bortliana.springmongo.services.UserService;
@@ -43,12 +44,16 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
-
      @RequestMapping (value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
         User obj = service.fromDTO(objDto);
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPost());
     }
 }
